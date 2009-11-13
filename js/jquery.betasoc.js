129d4adc8a2afa575e3cb00317f0639882c66a6e
ntulip <nicktulip@gmail.com>
2009-11-13 05:01:55 +0000

    initial plugin skeleton

---
+++ js/jquery.betasoc.js
@@ -0,0 +1,19 @@
+(function($) {
+
+	$.fn.betaSoc = function(settings) {
+
+		// extend passed in settings with defaults defined here
+		settings = jQuery.extend({
+			setting_name: "value"
+		}, settings);
+
+		function one(){
+
+		}
+
+		function two(){
+
+		}
+	}
+
+})(jQuery);
